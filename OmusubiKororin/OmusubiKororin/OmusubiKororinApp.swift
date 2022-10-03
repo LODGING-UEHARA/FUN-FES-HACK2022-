fd00983c6b54fd2881afe7437c56a626495cdfc6
Takumi130902 <b1020239@fun.jp>
2022-10-03 16:19:36 +0900

    AppSourceCode

---
+++ OmusubiKororin/OmusubiKororin/OmusubiKororinApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmusubiKororinApp.swift
+//  OmusubiKororin
+//
+//  Created by 児玉拓海 on 2022/10/03.
+//
+
+import SwiftUI
+
+@main
+struct OmusubiKororinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
